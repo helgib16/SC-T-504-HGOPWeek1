7d6eb13728db33fb53bc68f99b0fde4a81e1f823
helgi <helgib@ru.is>
2017-11-27 18:46:54 -0500

    working with some conditionals

--- V1D1.sh
+++ V1D1.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 # Basic bash script with walkthrough of installation 
 # of a text editor and git
-#timestamp(){
-#	date +"%T";
-#}
+
 ts=$(date +%s%N);
 echo "$(date +%d.%m.%Y-%T)";
 echo "Welcome $USER";
-echo "this program will guide you through the installation of a few prerequisites for the course SC-T-504-HGOP";
+echo "this program will install git and sublime text editor";
 echo "System running: ";
 uname -a;
 echo "are you sure you want to continue y/n?"
@@ -15,13 +13,41 @@ read input;
 if [ "$input" = "y" ];
 	then 
 	echo "Installing git..."
+	installGit() {
+		sudo apt-get install git;
+	}
 #	git --version
-	sudo apt-get install git
-	echo "Installing sublime text editor 3..."
+	#if [ installGit () ]
+	#	then
+	#	1>"logfile.txt";
+	#	echo "Successfully installed git"
+	#else
+	#	2>"errorfile.txt";
+	#	echo "Failed to install git, for more information check errorfile.txt"
+	#fi
+	#echo "Installing sublime text editor 3..."
+	installSublime(){
+		sudo add-apt-repository ppa:webupd8team/sublime-text-3 &&
+		sudo apt-get update &&
+		sudo apt-get install sublime-text-installer;
+	}
+	#if [ installSublime () ]
+	#then 
+	#	1>"logfile.txt";
+	#	echo "Successfully installed Sublime";
+	#else 
+	#	2>"errorfile.txt";
+	#	echo "Failed to install Sublime, for more information check errorfile.txt";
+#	if [ sudo add-apt-repository ppa:webupd8team/sublime-text-3 && \
+#		sudo apt-get update && \
+#		sudo apt-get install sublime-text-installer ]; then
+#		echo "All apps installed";
+	
 
-	sudo add-apt-repository ppa:webupd8team/sublime-text-3 && \
-	sudo apt-get update && \
-	sudo apt-get install sublime-text-installer
+#	else
+#		echo "Installing encountered an error"
+#	fi
+
 fi
 
 tt=$((($(date +%s%N) - $ts)/1000000))
