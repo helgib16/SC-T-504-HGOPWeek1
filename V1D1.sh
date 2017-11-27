ea383fb6f7639a413669b94ea09d3dd1f820aaf1
helgi <helgib@ru.is>
2017-11-27 11:50:56 -0500

    added week 1, day 1 file

---
+++ V1D1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Basic bash script with walkthrough of installation 
+# of a text editor and git
+#starttime="%T";
+#starttime;
+#timestamp(){
+#	date + "%T";
+#}
+echo 'Welcome $USER';
+echo 'this program will guide you through the installation of a few prerequisites for the course SC-T-504-HGOP';
+echo 'System running: ';
+uname -a;
